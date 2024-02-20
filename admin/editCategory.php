5ba63bca9f28fc1f9abd00a236d607c5d9c0b0c1
khforever <eng.kholoudmohamed@gmail.com>
2024-02-20 14:47:54 +0200

    edit

--- admin/editCategory.php
+++ admin/editCategory.php
@@ -310,24 +310,6 @@ if (isset($_POST['update'])) {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 									<form  method="POST" action="<?php echo $_SERVER["PHP_SELF"] ?>" id="demo-form2" data-parsley-validate class="form-horizontal form-label-left">
 
 										<div class="item form-group">
